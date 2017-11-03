22e8c42ece57441a8534963eabe024b4d3e2c316
Ronald Ray <fritz.ray@eduworks.com>
2017-11-03 14:00:02 -0700

    Better CSS linking

--- js/util.js
+++ js/util.js
@@ -34,6 +34,9 @@ $(document).ready(function () {
         var arrStyleSheets = parent.document.getElementsByTagName("style");
         for (var i = 0; i < arrStyleSheets.length; i++)
             oHead.appendChild(arrStyleSheets[i].cloneNode(true));
+        arrStyleSheets = parent.document.getElementsByTagName("link");
+        for (var i = 0; i < arrStyleSheets.length; i++)
+            oHead.appendChild(arrStyleSheets[i].cloneNode(true));
         try {
             importParentStyles();
         } catch (e) {}
