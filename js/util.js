0fc551d63d7687198d72afbeb605936e7b911d38
Ronald Ray <fritz.ray@eduworks.com>
2017-11-03 13:54:15 -0700

    Fixed stylesheet import

--- js/util.js
+++ js/util.js
@@ -30,11 +30,13 @@ jQuery.fn.outerHTML = function () {
 
 $(document).ready(function () {
     if (parent != window) {
-        importParentStyles();
         var oHead = document.getElementsByTagName("head")[0];
         var arrStyleSheets = parent.document.getElementsByTagName("style");
         for (var i = 0; i < arrStyleSheets.length; i++)
             oHead.appendChild(arrStyleSheets[i].cloneNode(true));
+        try {
+            importParentStyles();
+        } catch (e) {}
     }
 });
 
