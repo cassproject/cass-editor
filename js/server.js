3b7181b1a38b254d7ad01b0ef9cb1a31d29d9d7e
Mile Divovic <mile.divovic@eduworks.com>
2018-05-23 15:29:33 -0700

    Added check for bad message to highlighted message listener #290

--- js/server.js
+++ js/server.js
@@ -204,15 +204,14 @@ function cappend(event) {
     } else if (event.data.message == "back") {
         gotoPage('#editFrameworkSection');
     } else if (event.data.message == "highlighted") {
+        if (!event.data.highlightTarget)
+            return;
         var elem = $('[id="' + event.data.highlightTarget + '"]');
         if (elem.length && elem.is(':visible')) {
             $('[id="' + event.data.highlightTarget + '"]').click();
             $('#tree').animate({
                 scrollTop: elem.offset().top
             }, 1000);
-        } else {
-            console.log("Highlight failed");
-            console.log(event.data);
         }
     }
 }
