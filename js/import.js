f8a508128788fbd23eb1dcca8722d4929bacbc6b
Kristin <kristin.wood@eduworks.com>
2019-01-10 10:22:33 -0500

    File upload works while server is still returning /ctdlasn/ in URL

--- js/import.js
+++ js/import.js
@@ -374,6 +374,11 @@ function importJsonLdFramework() {
         processData: false,
         contentType: false,
         success: function (data) {
+            if (data.indexOf("ctdlasn") != -1) {
+                var data1 = data.substring(0, data.indexOf("ctdlasn"));
+                var data2 = data.substring(data.indexOf("ctdlasn")+7);
+                data = data1 + "data" + data2;
+            }
             framework = EcFramework.getBlocking(data);
             showPage("framework");
             populateFramework();
