7fc1c0dbdc865766c26fcd685e30b0fc2915132f
Kristin <kristin.wood@eduworks.com>
2019-12-24 12:01:34 -0500

    Remaining fields on framework list page

--- src/views/framework/Frameworks.vue
+++ src/views/framework/Frameworks.vue
@@ -6,18 +6,50 @@
             :click="frameworkClick">
             <template
                 v-slot:frameworkTags="slotProps">
-                {{ slotProps.item.competency.length }} competencies
+                <span>{{ slotProps.item.competency.length }} items </span>
+                <span
+                    v-if="slotProps.item.Published"
+                    :title="slotProps.item.Published">Published </span>
+                <span
+                    v-if="slotProps.item.Approved"
+                    :title="slotProps.item.Approved">Approved </span>
+                <span
+                    v-if="slotProps.item['schema:dateCreated']"
+                    :title="new Date(slotProps.item['schema:dateCreated'])">
+                    Created {{ $moment(slotProps.item['schema:dateCreated']).fromNow() }}
+                </span>
+                <span
+                    v-if="slotProps.item.getTimestamp()"
+                    :title="new Date(slotProps.item.getTimestamp())">
+                    Last modified {{ $moment(slotProps.item.getTimestamp()).fromNow() }}
+                </span>
+                <span
+                    v-else-if="slotProps.item['schema:dateModified']"
+                    :title="new Date(slotProps.item['schema:dateModified'])">
+                    Last modified {{ $moment(slotProps.item['schema:dateModified']).fromNow() }}
+                </span>
+                <span v-if="slotProps.item['ceasn:publisherName']">
+                    {{ getName(slotProps.item['ceasn:publisherName']) }}
+                </span>
+                <span v-else-if="slotProps.item['schema:publisher']">
+                    {{ getName(slotProps.item['schema:publisher']) }}
+                </span>
+                <span v-else-if="slotProps.item['schema:creator']">
+                    {{ getName(slotProps.item['schema:creator']) }}
+                </span>
             </template>
         </List>
     </div>
 </template>
 <script>
 import List from '@/lode/components/lode/List.vue';
+import common from '@/mixins/common.js';
 export default {
     name: "Frameworks",
     props: {
         queryParams: Object
     },
+    mixins: [common],
     data: function() {
         return {
             repo: window.repo
@@ -27,6 +59,14 @@ export default {
     methods: {
         frameworkClick: function(framework) {
             this.$router.push({name: "framework", params: {frameworkId: framework.id}});
+        },
+        getName: function(field) {
+            let name = EcArray.isArray(field) ? field : [field];
+            if (Thing.getDisplayStringFrom(name).toLowerCase().indexOf("http") !== -1) {
+                return this.resolveNameFromUrl(Thing.getDisplayStringFrom(name));
+            } else {
+                return Thing.getDisplayStringFrom(name);
+            }
         }
     }
 };
