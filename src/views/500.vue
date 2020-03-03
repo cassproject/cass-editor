13c71cbf9d46f401ea41b682d29ab3facf5af700
Aleita <traina@oregonstate.edu>
2020-03-03 14:06:44 -0800

    TPP-269

---
+++ src/views/500.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="internal-error">
+        <div class="section">
+            <h2 class="title is-size-3">
+                Internal Error
+            </h2>
+            <p>
+                The CaSS server is not responding properly. This is probably our fault. Please try again later or contact your system administrator.
+            </p>
+        </div>
+        <div class="section">
+            <div class="buttons is-centered">
+                <router-link to="/" class="button is-primary is-centered">
+                    return home
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: '500'
+};
+</script>
+
+<style scoped>
+#internal-error {
+    max-width: 800px;
+    display: block;
+    margin: auto;
+    height: 100%;
+    flex-direction: column;
+    justify-content: center;
+}
+</style>
