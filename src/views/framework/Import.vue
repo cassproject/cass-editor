0b8330e14324a83e454a94feb4b9bfe12042d1ac
Aleita <traina@oregonstate.edu>
2020-04-28 14:46:54 -0700

    TPP-366

--- src/views/framework/Import.vue
+++ src/views/framework/Import.vue
@@ -56,8 +56,7 @@
                         CaSS has detected a framework!
                     </span>
                     <br><br>
-                    We've gathered details about your competency framework and file.  Please review. Accept and approve to continue, cancel to review your file and re-import.
-                </p>
+                    Please review the competency framework and file details gathered below. To continue with the input of this competency framework, press Accept Details & Review. To cancel and review or change your input file, press cancel.                </p>
                 <p
                     v-if="importTransition === 'preview'"
                     class="">
