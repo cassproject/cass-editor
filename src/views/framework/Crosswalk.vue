19f5dddefd4293a5737e15c98d7f07a3adbd08dd
Aleita <traina@oregonstate.edu>
2020-05-05 13:27:27 -0700

    CA-40

--- src/views/framework/Crosswalk.vue
+++ src/views/framework/Crosswalk.vue
@@ -1,47 +1,76 @@
 <template>
     <div class="crosswalk section">
         <div class="container is-fluid">
-            <h2>Framework Crosswalk</h2>
             <div class="columns is-multiline">
                 <div class="column is-12 crosswalk__steps">
-                    <div class="columns is-mobile">
-                        <div class="column is-3" v-for="(step, index) in steps" :key="index">
-                            <div class="step-icon__wrapper has-text-centered">
-                                
-                                <div class="step-icon icon is-large has-background-primary has-text-white">
-                                    <h3 v-if="step.name === 'from'" class="has-text-white">A</h3>
-                                    <h3  v-if="step.name === 'to'" class="has-text-white">A</h3>
-                                    <i v-if="step.name === 'align'"  class="fa fa-network-wired"></i>
-                                    <i v-if="step.name === 'review'" class="fa fa-check"></i>
+                    <div class="section">
+                        <div class="container">
+                            <h2 class="title is-size-1">
+                                Framework Crosswalk
+                            </h2>
+                            <div class="columns is-mobile">
+                                <div
+                                    class="column is-3"
+                                    v-for="(step, index) in steps"
+                                    :key="index">
+                                    <div class="step-icon__wrapper has-text-centered">
+                                        <div
+                                            class="step-icon icon is-large has-text-white"
+                                            :class="[{'has-background-primary': step.complete === false}, { 'has-background-success': step.complete}]">
+                                            <h3
+                                                v-if="step.name === 'from'"
+                                                class="has-text-white">
+                                                A
+                                            </h3>
+                                            <h3
+                                                v-if="step.name === 'to'"
+                                                class="has-text-white">
+                                                A
+                                            </h3>
+                                            <i
+                                                v-if="step.name === 'align'"
+                                                class="fa fa-network-wired" />
+                                            <i
+                                                v-if="step.name === 'review'"
+                                                class="fa fa-check" />
+                                        </div>
+                                        <p class="label is-size-4 has-text-primary has-text-centered">
+                                            <span
+                                                v-if="step.complete"
+                                                class="icon">
+                                                <i class="fa fa-check" />
+                                            </span> {{ step.description }}
+                                        </p>
+                                    </div>
                                 </div>
-                                <p class="label is-size-4 has-text-primary has-text-centered">
-                                <span v-if="step.complete" class="icon">
-                                    <i class="fa fa-check"/>
-                                </span> {{ step.description }}
-                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- step framework list for selecting a & b --> 
-                <div v-if="step === 0 || step === 1" class="column is-12 crosswalk__list">
+                <!-- step framework list for selecting a & b -->
+                <div
+                    v-if="step === 0 || step === 1"
+                    class="column is-12 crosswalk__list">
                     <div class="section">
-                    <SearchBar searchType="framework" />
-                    <List 
-                        :type="type"
-                        :repo="repo"
-                        :click="frameworkClick"
-                        :searchOptions="searchOptions"
-                        :paramObj="paramObj"
-                        :disallowEdits="true"/>
+                        <SearchBar searchType="framework" />
+                        <List
+                            :type="type"
+                            :repo="repo"
+                            :click="frameworkClick"
+                            :searchOptions="searchOptions"
+                            :paramObj="paramObj"
+                            :disallowEdits="true" />
                     </div>
                 </div>
-                <!-- double hierarchy view --> 
-                <div v-if="step === 2" class="column is-12 crosswalk__double-hierarchy">
+                <!-- double hierarchy view -->
+                <div
+                    v-if="step === 2"
+                    class="column is-12 crosswalk__double-hierarchy">
                     <div class="columns">
                         <div class="column is-6 crosswalk__single-hierachy">
-                             <Hierarchy
+                            <Hierarchy
                                 :container="frameworkA"
+                                view="crosswalk"
                                 containerType="Framework"
                                 containerTypeGet="EcFramework"
                                 containerNodeProperty="competency"
@@ -55,21 +84,15 @@
                                 :viewOnly="queryParams.view === 'true'"
                                 :repo="repo"
                                 :queryParams="queryParams"
-                                :exportOptions="competencyExportOptions"
-                                :highlightList="highlightCompetency"
-                                :profile="competencyProfile"
-                                @deleteObject="deleteObject"
-                                @editMultipleEvent="onEditMultiple"
-                                @removeObject="removeObject"
-                                @exportObject="exportObject"
+                                :exportOptions="[]"
+                                :highlightList="null"
                                 @searchThings="handleSearch($event)"
-                                @selectButtonClick="onSelectButtonClick"
-                                :properties="properties"
-                                @selectedArray="selectedArrayEvent" />
+                                properties="primary" />
                         </div>
                         <div class="column is-6 crosswalk__single-hierachy">
-                             <Hierarchy
+                            <Hierarchy
                                 :container="frameworkB"
+                                view="crosswalk"
                                 containerType="Framework"
                                 containerTypeGet="EcFramework"
                                 containerNodeProperty="competency"
@@ -83,20 +106,13 @@
                                 :viewOnly="queryParams.view === 'true'"
                                 :repo="repo"
                                 :queryParams="queryParams"
-                                :exportOptions="competencyExportOptions"
-                                :highlightList="highlightCompetency"
-                                :profile="competencyProfile"
-                                @deleteObject="deleteObject"
-                                @editMultipleEvent="onEditMultiple"
-                                @removeObject="removeObject"
-                                @exportObject="exportObject"
+                                :exportOptions="[]"
+                                :highlightList="null"
                                 @searchThings="handleSearch($event)"
-                                @selectButtonClick="onSelectButtonClick"
-                                :properties="properties"
-                                @selectedArray="selectedArrayEvent" />
+                                properties="primary" />
                         </div>
                     </div>
-                 <!-- side by side view -->
+                    <!-- side by side view -->
                 </div>
             </div>
         </div>
@@ -147,32 +163,33 @@ export default {
         Hierarchy
     },
     mounted() {
+        this.$store.commit('crosswalk/step', 0);
     },
-    watch: { 
-        step: function( val) {
+    watch: {
+        step: function(val) {
             console.log("step, ", val);
             console.log("steps, ", this.steps);
-            if(val === 0) {
+            if (val === 0) {
                 this.steps[0].complete = false;
                 this.steps[1].complete = false;
                 this.steps[2].complete = false;
                 this.steps[3].complete = false;
-            } else if(val === 1) {
+            } else if (val === 1) {
                 this.steps[0].complete = true;
                 this.steps[1].complete = false;
                 this.steps[2].complete = false;
                 this.steps[3].complete = false;
-            } else if(val === 2) {
+            } else if (val === 2) {
                 this.steps[0].complete = true;
                 this.steps[1].complete = true;
                 this.steps[2].complete = false;
                 this.steps[3].complete = false;
-            } else if(val === 3) {
+            } else if (val === 3) {
                 this.steps[0].complete = true;
                 this.steps[1].complete = true;
                 this.steps[2].complete = true;
                 this.steps[3].complete = false;
-            } else if(val === 4) {
+            } else if (val === 4) {
                 this.steps[0].complete = true;
                 this.steps[1].complete = true;
                 this.steps[2].complete = true;
@@ -185,48 +202,48 @@ export default {
             return this.queryParams.concepts === 'true' ? "ConceptScheme" : "Framework";
         },
         paramObj: function() {
-        let obj = {};
-        obj.size = 20;
-        var order = (this.sortBy === "name.keyword" || this.sortBy === "dcterms:title.keyword") ? "asc" : "desc";
-        obj.sort = '[ { "' + this.sortBy + '": {"order" : "' + order + '" , "unmapped_type" : "long",  "missing" : "_last"}} ]';
-        if (this.queryParams && ((this.queryParams.concepts !== "true" && this.queryParams.show === 'mine') ||
+            let obj = {};
+            obj.size = 20;
+            var order = (this.sortBy === "name.keyword" || this.sortBy === "dcterms:title.keyword") ? "asc" : "desc";
+            obj.sort = '[ { "' + this.sortBy + '": {"order" : "' + order + '" , "unmapped_type" : "long",  "missing" : "_last"}} ]';
+            if (this.queryParams && ((this.queryParams.concepts !== "true" && this.queryParams.show === 'mine') ||
             (this.queryParams.concepts === "true" && this.queryParams.conceptShow === "mine"))) {
-            obj.ownership = 'me';
-        }
-        return obj;
-    },
-    searchOptions: function() {
-        let search = "";
-        if (this.queryParams && this.queryParams.filter != null) {
-            search += " AND (" + this.queryParams.filter + ")";
-        }
-        if (this.showMine || (this.queryParams && this.queryParams.concepts !== "true" && this.queryParams.show === "mine") ||
+                obj.ownership = 'me';
+            }
+            return obj;
+        },
+        searchOptions: function() {
+            let search = "";
+            if (this.queryParams && this.queryParams.filter != null) {
+                search += " AND (" + this.queryParams.filter + ")";
+            }
+            if (this.showMine || (this.queryParams && this.queryParams.concepts !== "true" && this.queryParams.show === "mine") ||
             (this.queryParams && this.queryParams.concepts === "true" && this.queryParams.conceptShow === "mine")) {
-            search += " AND (";
-            for (var i = 0; i < EcIdentityManager.ids.length; i++) {
-                if (i !== 0) {
-                    search += " OR ";
+                search += " AND (";
+                for (var i = 0; i < EcIdentityManager.ids.length; i++) {
+                    if (i !== 0) {
+                        search += " OR ";
+                    }
+                    var id = EcIdentityManager.ids[i];
+                    search += "@owner:\"" + id.ppk.toPk().toPem() + "\"";
+                    search += " OR @owner:\"" + this.addNewlinesToId(id.ppk.toPk().toPem()) + "\"";
                 }
-                var id = EcIdentityManager.ids[i];
-                search += "@owner:\"" + id.ppk.toPk().toPem() + "\"";
-                search += " OR @owner:\"" + this.addNewlinesToId(id.ppk.toPk().toPem()) + "\"";
+                search += ")";
             }
-            search += ")";
-        }
-        if (this.showNotMine) {
-            search += " AND NOT (";
-            for (var i = 0; i < EcIdentityManager.ids.length; i++) {
-                if (i !== 0) {
-                    search += " OR ";
+            if (this.showNotMine) {
+                search += " AND NOT (";
+                for (var i = 0; i < EcIdentityManager.ids.length; i++) {
+                    if (i !== 0) {
+                        search += " OR ";
+                    }
+                    var id = EcIdentityManager.ids[i];
+                    search += "@owner:\"" + id.ppk.toPk().toPem() + "\"";
+                    search += " OR @owner:\"" + this.addNewlinesToId(id.ppk.toPk().toPem()) + "\"";
                 }
-                var id = EcIdentityManager.ids[i];
-                search += "@owner:\"" + id.ppk.toPk().toPem() + "\"";
-                search += " OR @owner:\"" + this.addNewlinesToId(id.ppk.toPk().toPem()) + "\"";
+                search += ")";
             }
-            search += ")";
-        }
-        return search;
-    },
+            return search;
+        },
         ...mapState({
             step: state => state.crosswalk.step,
             sortResults: state => state.app.sortResults,
@@ -241,13 +258,13 @@ export default {
     methods: {
         frameworkClick: function(framework) {
             var me = this;
-            if(this.step === 0) {
+            if (this.step === 0) {
                 EcFramework.get(framework.id, function(success) {
                     me.$store.commit('crosswalk/frameworkA', success);
                     me.$store.commit('crosswalk/step', 1);
                 }, console.error);
             } else {
-                if(this.step === 1) {
+                if (this.step === 1) {
                     /* Should we exclude framework A from framework B options */
                     EcFramework.get(framework.id, function(success) {
                         me.$store.commit('crosswalk/frameworkB', success);
@@ -255,7 +272,6 @@ export default {
                     }, console.error);
                 }
             }
-            
         }
     }
 };
@@ -273,7 +289,7 @@ export default {
         border-radius: 100% !important;
     }
     .crosswalk__steps {
-        height: 10vh;
+        height: 18vh;
     }
     .crosswalk__list {
         overflow: scroll;
