337688011dbc6f82979a9dc0997c5fa5ab0f0a63
Kristin <kristin.wood@eduworks.com>
2020-05-14 16:50:05 -0400

    Use framework from store for remove and delete; var names have changed
    CA-367

--- src/mixins/competencyEdits.js
+++ src/mixins/competencyEdits.js
@@ -4,24 +4,25 @@ export default {
             // Remove from container but don't delete
             console.log("removing " + thing.id);
             var me = this;
-            this.framework["schema:dateModified"] = new Date().toISOString();
-            this.framework.removeCompetency(thing.shortId(), function() {
+            var thisFramework = this.$store.getters['editor/framework'];
+            thisFramework["schema:dateModified"] = new Date().toISOString();
+            thisFramework.removeCompetency(thing.shortId(), function() {
                 var framework = me.framework;
                 if (me.$store.state.editor.private === true && EcEncryptedValue.encryptOnSaveMap[f.id] !== true) {
                     framework = EcEncryptedValue.toEncryptedValue(framework);
                 }
                 repo.saveTo(framework, function() {
                     me.confirmDialog = false;
-                    me.$store.commit('editor/framework', me.framework);
+                    me.$store.commit('editor/framework', thisFramework);
                 }, console.error);
             }, console.log);
         },
         deleteObject: function(thing) {
             console.log("deleting " + thing.id);
             var me = this;
-            if (thing.shortId() === this.framework.shortId()) {
+            var framework = this.$store.getters['editor/framework'];
+            if (thing.shortId() === framework.shortId()) {
                 // delete framework
-                var framework = this.framework;
                 this.repo.deleteRegistered(framework, function(success) {
                     me.spitEvent("frameworkDeleted", framework.shortId(), "editFrameworkSection");
                     // Delete the framework, delete all non-used stuff.
@@ -45,26 +46,26 @@ export default {
                 }, console.log);
             } else {
                 // Delete competency and relations
-                var initialCompetencies = this.framework.competency ? this.framework.competency.slice() : null;
-                var initialRelations = this.framework.relation ? this.framework.relation.slice() : null;
-                var initialLevels = this.framework.level ? this.framework.level.slice() : null;
+                var initialCompetencies = framework.competency ? framework.competency.slice() : null;
+                var initialRelations = framework.relation ? framework.relation.slice() : null;
+                var initialLevels = framework.level ? framework.level.slice() : null;
                 this.$store.commit('editor/selectedCompetency', thing);
-                this.framework["schema:dateModified"] = new Date().toISOString();
-                this.framework.removeCompetency(thing.shortId(), function() {
-                    me.framework.removeLevel(thing.shortId());
+                framework["schema:dateModified"] = new Date().toISOString();
+                framework.removeCompetency(thing.shortId(), function() {
+                    framework.removeLevel(thing.shortId());
                     me.$store.commit('editor/addEditsToUndo', [
                         {operation: "delete", obj: thing},
-                        {operation: "update", id: me.framework.shortId(), fieldChanged: ["competency", "relation", "level"], initialValue: [initialCompetencies, initialRelations, initialLevels], changedValue: [me.framework.competency, me.framework.relation, me.framework.level]}
+                        {operation: "update", id: framework.shortId(), fieldChanged: ["competency", "relation", "level"], initialValue: [initialCompetencies, initialRelations, initialLevels], changedValue: [framework.competency, framework.relation, framework.level]}
                     ]);
                     me.conditionalDelete(thing.shortId());
                     me.spitEvent("competencyDeleted", thing.shortId(), "editFrameworkSection");
                     me.$store.commit('editor/selectedCompetency', null);
-                    var framework = me.framework;
-                    if (me.$store.state.editor.private === true && EcEncryptedValue.encryptOnSaveMap[me.framework.id] !== true) {
-                        framework = EcEncryptedValue.toEncryptedValue(framework);
+                    var frameworkToSave = framework;
+                    if (me.$store.state.editor.private === true && EcEncryptedValue.encryptOnSaveMap[framework.id] !== true) {
+                        frameworkToSave = EcEncryptedValue.toEncryptedValue(framework);
                     }
-                    me.repo.saveTo(framework, function() {
-                        me.$store.commit('editor/framework', me.framework);
+                    me.repo.saveTo(frameworkToSave, function() {
+                        me.$store.commit('editor/framework', framework);
                     }, console.error);
                 }, console.log);
             }
