cfca9d16ebe7ac221ea6219593534afba4627065
Aleita <traina@oregonstate.edu>
2020-06-01 13:20:10 -0700

    welcome page

---
+++ src/views/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="section">
+        <div class="container">
+            <h1 class="title is-size-1">
+                Welcome to cass
+            </h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Welcome'
+};
+</script>
