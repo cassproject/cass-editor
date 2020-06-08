df972f87f51d9271a7a2cd029efb67d504f841d4
Kristin <kristin.wood@eduworks.com>
2020-06-08 15:44:00 -0400

    Compute relations & levels before they're needed 2 speed up view switch
    CA-384

--- src/views/framework/Framework.vue
+++ src/views/framework/Framework.vue
@@ -84,7 +84,8 @@
                             @searchThings="handleSearch($event)"
                             @selectButtonClick="onSelectButtonClick"
                             :properties="properties"
-                            @selectedArray="selectedArrayEvent" />
+                            @selectedArray="selectedArrayEvent"
+                            @doneLoadingNodes="preloadRelations" />
                     </div>
                 </div>
             </div>
@@ -671,6 +672,11 @@ export default {
             };
             // reveal modal
             this.$modal.show(params);
+        },
+        // Speed up load of secondary properties
+        preloadRelations: function() {
+            var relation = this.relations;
+            var level = this.levels;
         }
     }
 };
