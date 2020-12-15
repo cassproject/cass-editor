a412c07aae2b1e42bc8a98376dc3bf20c18b54e7
Kristin <kristin.wood@eduworks.com>
2020-12-15 13:09:37 -0600

    Show basic directory details in RightAside

--- src/components/framework/ListItemInfo.vue
+++ src/components/framework/ListItemInfo.vue
@@ -7,6 +7,20 @@
                 {{ objectName }}
             </h3>
         </div>
+        <div>
+            Details
+            <div>Last Modified: {{ lastModified }}</div>
+            <div>Date Created: {{ dateCreated }}</div>
+            <div>Subdirectories: {{ numSubdirectories }}</div>
+            <div>Objects: {{ numObjects }}</div>
+        </div>
+        <div>
+            Description
+            <div>{{ objectDescription }}</div>
+        </div>
+        <button @click="openObject">
+            Open
+        </button>
     </aside>
 </template>
 <script>
@@ -17,24 +31,80 @@ export default {
     mixins: [common],
     data() {
         return {
+            numSubdirectories: "unknown",
+            numObjects: "unknown"
         };
     },
     methods: {
+        setNumSubdirectoriesAndObjects: function() {
+            let me = this;
+            if (this.objectType === "Directory") {
+                window.repo.searchWithParams("(@type:Directory AND parentDirectory:\"" + this.object.shortId() + "\")", {'size': 10000}, null, function(results) {
+                    me.numSubdirectories = results.length;
+                    if (results.length === 10000) {
+                        me.numSubdirectories = "More than 10,000";
+                    }
+                }, function(error) {
+                    appError(error);
+                    me.numSubdirectories = "unknown";
+                });
+                window.repo.searchWithParams("(directory:\"" + this.object.shortId() + "\")", {'size': 10000}, null, function(results) {
+                    me.numObjects = results.length;
+                    if (results.length === 10000) {
+                        me.numObjects = "More than 10,000";
+                    }
+                }, function(error) {
+                    appError(error);
+                    me.numObjects = "unknown";
+                });
+            }
+        },
+        openObject: function() {
+            if (this.objectType === "Directory") {
+                this.$store.commit('app/selectDirectory', this.object);
+                if (this.$route.name !== "directory") {
+                    this.$router.push({name: "directory"});
+                }
+                this.$store.commit('app/closeRightAside');
+            }
+        }
+    },
+    mounted: function() {
+        this.setNumSubdirectoriesAndObjects();
     },
     computed: {
         objectName: function() {
             return Thing.getDisplayStringFrom(this.object.name);
+        },
+        objectDescription: function() {
+            return Thing.getDisplayStringFrom(this.object.description);
+        },
+        objectShortId: function() {
+            return this.object.shortId();
         },
         object: function() {
             return this.$store.getters['app/rightAsideObject'];
         },
         objectType: function() {
             return this.object.type;
+        },
+        lastModified: function() {
+            if (this.object["schema:dateModified"]) {
+                return this.$moment(new Date(this.object['schema:dateModified'])).format("MMM D YYYY");
+            }
+            return "unknown";
+        },
+        dateCreated: function() {
+            if (this.object["schema:dateCreated"]) {
+                return this.$moment(new Date(this.object['schema:dateCreated'])).format("MMM D YYYY");
+            }
+            return "unknown";
         }
     },
     watch: {
-    },
-    mounted: function() {
+        objectShortId: function() {
+            this.setNumSubdirectoriesAndObjects();
+        }
     }
 };
 </script>
