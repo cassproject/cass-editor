bb82f39f1ee369c10146b0c9a0e529692aa610fa
Kristin <kristin.wood@eduworks.com>
2021-02-11 12:52:42 -0600

    Watcher was lost in merge CASS-505

--- src/views/directory/Directory.vue
+++ src/views/directory/Directory.vue
@@ -207,7 +207,7 @@
                             :key="each.id">
                             <a
                                 href="#"
-                                @click="$store.commit('app/selectDirectory', each)">{{ each.name + ' /' }}</a>
+                                @click="$store.commit('app/selectDirectory', each)">{{ each.name }}</a>
                         </li>
                         <li>
                             <a
@@ -802,6 +802,12 @@ export default {
             } else {
                 this.resourceName = '';
                 this.resourceUrl = '';
+            }
+        },
+        directory: function() {
+            if (this.directory) {
+                this.directoryTrail.splice(0, this.directoryTrail.length);
+                this.findDirectoryTrail(this.directory);
             }
         }
     }
