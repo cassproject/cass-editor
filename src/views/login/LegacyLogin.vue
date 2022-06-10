23605c2715c885a01176a84c4ff072cd88c7d7e6
Fritz Ray <fritz.ray@eduworks.com>
2022-06-10 11:43:22 -0700

    Removed console.log

--- src/views/login/LegacyLogin.vue
+++ src/views/login/LegacyLogin.vue
@@ -609,7 +609,6 @@ export default {
             } else this.findLinkedPersonForIdentity();
         },
         handleAttemptLoginFetchIdentityFailure: function(failMsg) {
-            console.error(failMsg);
             this.identityFailMsg = failMsg;
             this.identityFetchFailed = true;
             this.loginBusy = false;
