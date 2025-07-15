8030687ed607d41689c5d26b1a0866683d9036a9
Mile Divovic <mile.divovic@eduworks.com>
2025-07-15 12:54:51 -0700

    add ceterms:versionCode

--- src/mixins/ctdlasnProfile.js
+++ src/mixins/ctdlasnProfile.js
@@ -493,6 +493,20 @@ export default {
                     "max": 1,
                     "heading": "Tagging"
                 },
+                "https://purl.org/ctdl/terms/versionCode": {
+                    "@id": "https://purl.org/ctdl/terms/versionCode",
+                    "@type": ["http://www.w3.org/2000/01/rdf-schema#Property"],
+                    "http://schema.org/domainIncludes":
+                    [{"@id": "https://schema.cassproject.org/0.4/Framework"}],
+                    "http://schema.org/rangeIncludes": [{"@id": "http://schema.org/Text"}],
+                    "http://www.w3.org/2000/01/rdf-schema#comment":
+                    [
+                        {"@language": "en",
+                            "@value": `Alphanumeric identifier of the version of the resource that is unique within the organizational context of its owner and which does not need the context of other information in order to be interpreted.`}
+                    ],
+                    "http://www.w3.org/2000/01/rdf-schema#label": [{"@language": "en", "@value": "Version Code"}],
+                    "heading": "Tagging"
+                },
                 "headings": ["General", "Tagging", "Connections", "Context", "Rights", "Keys"],
                 "primaryProperties": [
                     "http://schema.org/name",
@@ -518,7 +532,8 @@ export default {
                     "https://purl.org/ctdl/terms/versionIdentifier",
                     "https://purl.org/ctdl/terms/latestVersion",
                     "https://purl.org/ctdl/terms/previousVersion",
-                    "https://purl.org/ctdl/terms/nextVersion"
+                    "https://purl.org/ctdl/terms/nextVersion",
+                    "https://purl.org/ctdl/terms/versionCode"
                 ],
                 "tertiaryProperties": [
                     "@id",
@@ -851,6 +866,20 @@ export default {
                     "max": 1,
                     "heading": "Tagging"
                 },
+                "https://purl.org/ctdl/terms/versionCode": {
+                    "@id": "https://purl.org/ctdl/terms/versionCode",
+                    "@type": ["http://www.w3.org/2000/01/rdf-schema#Property"],
+                    "http://schema.org/domainIncludes":
+                    [{"@id": "https://schema.cassproject.org/0.4/Framework"}],
+                    "http://schema.org/rangeIncludes": [{"@id": "http://schema.org/Text"}],
+                    "http://www.w3.org/2000/01/rdf-schema#comment":
+                    [
+                        {"@language": "en",
+                            "@value": `Alphanumeric identifier of the version of the resource that is unique within the organizational context of its owner and which does not need the context of other information in order to be interpreted.`}
+                    ],
+                    "http://www.w3.org/2000/01/rdf-schema#label": [{"@language": "en", "@value": "Version Code"}],
+                    "heading": "Tagging"
+                },
                 "headings": ["General", "Tagging", "Keys"],
                 "primaryProperties": [
                     "http://schema.org/name",
@@ -875,7 +904,8 @@ export default {
                     "https://purl.org/ctdl/terms/versionIdentifier",
                     "https://purl.org/ctdl/terms/latestVersion",
                     "https://purl.org/ctdl/terms/previousVersion",
-                    "https://purl.org/ctdl/terms/nextVersion"
+                    "https://purl.org/ctdl/terms/nextVersion",
+                    "https://purl.org/ctdl/terms/versionCode"
                 ],
                 "tertiaryProperties": [
                     "@id",
@@ -1655,6 +1685,20 @@ export default {
                     "max": 1,
                     "heading": "Tagging"
                 },
+                "https://purl.org/ctdl/terms/versionCode": {
+                    "@id": "https://purl.org/ctdl/terms/versionCode",
+                    "@type": ["http://www.w3.org/2000/01/rdf-schema#Property"],
+                    "http://schema.org/domainIncludes":
+                    [{"@id": "https://schema.cassproject.org/0.4/Competency"}],
+                    "http://schema.org/rangeIncludes": [{"@id": "http://schema.org/Text"}],
+                    "http://www.w3.org/2000/01/rdf-schema#comment":
+                    [
+                        {"@language": "en",
+                            "@value": `Alphanumeric identifier of the version of the resource that is unique within the organizational context of its owner and which does not need the context of other information in order to be interpreted.`}
+                    ],
+                    "http://www.w3.org/2000/01/rdf-schema#label": [{"@language": "en", "@value": "Version Code"}],
+                    "heading": "Tagging"
+                },
                 "headings": ["General", "Context", "Tagging", "Scales", "Connections", "Keys"],
                 "primaryProperties": [
                     "https://purl.org/ctdlasn/terms/competencyLabel",
@@ -1683,7 +1727,8 @@ export default {
                     "https://purl.org/ctdl/terms/versionIdentifier",
                     "https://purl.org/ctdl/terms/latestVersion",
                     "https://purl.org/ctdl/terms/previousVersion",
-                    "https://purl.org/ctdl/terms/nextVersion"
+                    "https://purl.org/ctdl/terms/nextVersion",
+                    "https://purl.org/ctdl/terms/versionCode"
                 ],
                 "tertiaryProperties": [
                     "@id",
