d9865ad5e1dc6e5bd9bec1ee355aa3d2f84ae765
Fritz Ray <fritz.ray@eduworks.com>
2025-09-22 13:24:04 -0700

    e2e test updates.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,18 +2,20 @@ describe('template spec', () => {
 })
 
 /* ==== Test Created with Cypress Studio ==== */
-it('create and delete framework', function() {
+it('create and delete framework', function () {
   /* ==== Generated with Cypress Studio ==== */
-  cy.visit('http://localhost:8082?server=http://dev.cassproject.org/api/');
-  cy.get(':nth-child(2) > :nth-child(1) > a > :nth-child(2)').click();
-  cy.get('.dropdown-trigger > .is-hidden-touch > :nth-child(2)').click();
-  cy.get('[href="#"]').click();
-  cy.get('#property-string-computed-text').click();
-  cy.get('#property-string-computed-text').click();
-  cy.get('#property-string-computed-text').click();
-  cy.get('.is-dark > :nth-child(2)').click();
-  cy.get('.edit-button > .icon > .fa').click();
-  cy.get('.button.is-danger > .icon > .fa').click();
-  cy.get('#app > :nth-child(1) > #cass-modal > .modal-card > .modal-card-foot > .is-danger').click();
+  cy.visit('http://localhost:8082?server=https://dev.cassproject.org/api/');
+  cy.get('#side-nav-frameworks-link').click();
+  cy.get('#add-new-dropdown-toggle-button').click();
+  cy.get('#add-new-dropdown-framework').click();
+  cy.get('section.modal-card-body').click();
+  cy.get('#http\\:\\/\\/schema\\.org\\/description-0 > .field > .control.is-expanded > #property-string-computed-text').click();
+  cy.get('#http\\:\\/\\/schema\\.org\\/description-0 > .field > .control.is-expanded > #property-string-computed-text').clear('');
+  cy.get('#http\\:\\/\\/schema\\.org\\/description-0 > .field > .control.is-expanded > #property-string-computed-text').type("This is a test framework created by Cypress. It should be deleted.");
+  cy.get('#thing-editing-done-editing-button').click();
+  cy.get('#edit-node-button').click({ force: true }); // The window should be scrolled such that this is visible. It may be visible, maybe not. This is a design principle issue?
+  cy.wait(2000); // This is a bug, it should be either invisible while binding or bound immediately.
+  cy.get('#cancel-create-new-thing-button').click();
+  cy.get('#delete-framework-confirm-delete-button').click();
   /* ==== End Cypress Studio ==== */
 });
