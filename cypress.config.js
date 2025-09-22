27910d3a7f3eb838a397e58d72a51fcd579facc1
Fritz Ray <fritz.ray@eduworks.com>
2025-09-22 13:24:04 -0700

    e2e test updates.

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     experimentalStudio: true,
+    defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,12 @@
 
   it('createAccount', () => {
-    cy.visit('http://localhost:8082/?server=http://dev.cassproject.org/api/')
+    cy.visit('http://localhost:8082/?server=https://dev.cassproject.org/api/');
 
-    let rando = Math.floor(Math.random() * 1000000)
-    let user = `testuser${rando}`
-    let email = `testuser${rando}@example.com`
-    let password = 'testpassword'
+    let rando = Math.floor(Math.random() * 1000000);
+    let user = `testuser${rando}`;
+    let email = `testuser${rando}@example.com`;
+    let password = 'testpassword';
+
     /* ==== Generated with Cypress Studio ==== */
     cy.get('#side-nav-show-login-button').click();
     cy.get('#legacy-login-create-account-button').click();
@@ -20,25 +21,26 @@
     cy.get('#legacy-login-create-account-password-confirm').clear();
     cy.get('#legacy-login-create-account-password-confirm').type(password);
     cy.get('#legacy-login-create-link-person-button').click();
-    cy.get('.logo-container > .button > span').click();
-    cy.get('.px-3 > .my-2 > .button').click();
-    cy.get('.modal-card-foot > .buttons > .is-primary > :nth-child(2)').click();
-    cy.get('#legacy-login-username').clear();
+    cy.get('#side-nav-user-icon-button').click();
+    cy.get('#side-nav-logout-button').click();
+    cy.get('#logout-success-login-button').click();
+    cy.get('#legacy-login-username').clear('');
     cy.get('#legacy-login-username').type(user);
-    cy.get('#legacy-login-password').clear();
-    cy.get('#legacy-login-password').type(password);
-    cy.get('#legacy-login-button > :nth-child(2)').click();
-    cy.get('.logo-container > .button > span').click();
-    cy.get('.my-2 > .button > :nth-child(2)').click();
-    cy.get('.is-secondary > :nth-child(2)').click();
-    cy.get('#side-nav-show-login-button').click();
-    cy.get('#legacy-login-username').clear();
-    cy.get('#legacy-login-username').type(user);
-    cy.get('#legacy-login-password').clear();
+    cy.get('#legacy-login-password').clear('');
     cy.get('#legacy-login-password').type(password);
     cy.get('#legacy-login-button').click();
-    cy.get('.logo-container > .button > span').click();
-    cy.get('.my-2 > .button > :nth-child(2)').click();
-    cy.get('.is-secondary > :nth-child(2)').click();
+    cy.get('#side-nav-user-icon-button').click();
+    cy.get('#side-nav-logout-button').click();
+    cy.get('#logout-success-close-button').click();
+    cy.get('#side-nav-user-icon-button').click();
+    cy.get('#side-nav-show-login-button').click();
+    cy.get('#legacy-login-username').clear('');
+    cy.get('#legacy-login-username').type(user);
+    cy.get('#legacy-login-password').clear('');
+    cy.get('#legacy-login-password').type(password);
+    cy.get('#legacy-login-button').click();
+    cy.get('#side-nav-user-icon-button').click();
+    cy.get('#side-nav-logout-button').click();
+    cy.get('#logout-success-close-button').click();
     /* ==== End Cypress Studio ==== */
   })
