ed5300ec1f2bf7fa313811cf14f1f5184bddc44b
Fritz Ray <fritz.ray@eduworks.com>
2026-02-27 08:29:06 -0800

    Auto stash before merge of "test-coverage" and "e2e-tests"

--- playwright.config.js
+++ playwright.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
     testDir: './tests/e2e',
     /* Run tests in files in parallel */
     fullyParallel: true,
+    //maxFailures: 1,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
